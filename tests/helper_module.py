8368de6d24e7bdc0c86d36f40da4fc24add76724
ShadowGlass0 <gkecskem@gmail.com>
2025-06-23 09:54:10 +1000

    Add expansion_function support for wildcard expansion (#148)

--- tests/helper_module.py
+++ tests/helper_module.py
@@ -11,3 +11,9 @@ def fetch(symbol: str) -> None:
 def combine(symbol: str, codes: list[str]) -> None:
     # Dummy combine function used for tests
     pass
+
+
+def expand_codes(symbol: str) -> list[dict[str, str]]:
+    """Return two dummy contract codes for expansion tests."""
+    _ = symbol
+    return [{"code": "AA"}, {"code": "BB"}]
