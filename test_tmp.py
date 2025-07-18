0fb947b9808992985dfec1a4b02668b3bca1ebcc
gkecskem <gkecskem@gmail.com>
2025-07-19 01:25:37 +1000

    Added torch.compile to step method

--- test_tmp.py
+++ test_tmp.py
@@ -338,7 +338,7 @@ env = ifera.SingleMarketEnv(
     device=torch.device("cuda:0"),
     dtype=torch.float32,
 )
-batch_size = 50 # env.instrument_data.data.shape[0] - 250
+batch_size = env.instrument_data.data.shape[0] - 250
 
 openPolicy = ifera.OpenOncePolicy(direction=1, batch_size=batch_size, device=env.device)
 initStopPolicy = ifera.InitialArtrStopLossPolicy(
@@ -371,7 +371,7 @@ print(f"Starting simulation for {symbol}")
 t = time.time()
 
 total_profit = env.rollout(
-    trading_policy=tradingPolicy, start_date_idx=date_idx, start_time_idx=time_idx, max_steps=2000
+    trading_policy=tradingPolicy, start_date_idx=date_idx, start_time_idx=time_idx, max_steps=10000
 )
 # total_profit = env.rollout_with_display(
 #     trading_policy=tradingPolicy, start_date_idx=date_idx, start_time_idx=time_idx, max_steps=2000
