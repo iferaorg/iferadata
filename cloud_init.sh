397dd40c2fe1c3f2d10b54027b226ab93142ea65
gkecskem <gkecskem@gmail.com>
2025-09-11 14:33:01 +1000

    Added cloud init script to automate initial setup of cloud instances.

---
+++ cloud_init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+sudo apt update
+sudo snap install aws-cli --classic
+
+aws configure
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b
+export PATH="$HOME/miniconda3/bin:$PATH"
+conda init
+
+git clone https://github.com/iferaorg/iferadata.git
+cd iferadata
+
+pip install -r requirements.txt
+
+export DATA_FOLDER="data"
+export S3_BUCKET="ifera-marketdata"
+
+# Ask for github token
+read -p "Enter your GitHub token: " GITHUB_TOKEN
